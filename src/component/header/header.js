99acd251b5b7b18ebaa19c038d5e744e6d56438b
jwchan <741755613@qq.com>
2020-07-07 11:23:03 +0800

    feat(Header): user info

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { message } from 'antd'
 import { UserOutlined, AndroidOutlined, LogoutOutlined } from '@ant-design/icons'
-import { removeStorage } from 'common/js/localstorage'
+import { getStorage, removeStorage } from 'common/js/localstorage'
 
 import styles from './header.styl'
 
@@ -14,16 +14,43 @@ class Header extends React.Component {
   }
 
   render() {
+    const user = getStorage('user')
+    if(user === ''){
+      this.props.history.push({
+        pathname: '/login'
+      })
+      message.info('请先登录')
+      return null
+    }
+
+    const roleName = () => {
+      if(user.identity == 1){
+        return '超级管理员'
+      }else if(user.identity == 2){
+        return '管理员'
+      }else if(user.identity == 3){
+        return '运营'
+      }else if(user.identity == 4){
+        return '版主'
+      }else{
+        return '普通用户'
+      }
+    }
+
+    const nickName = () => {
+      return user.uname
+    }
+
     return (
       <div className={styles.header}>
         <div className={styles.user}>
           <div className={styles.role}>
             <AndroidOutlined />
-            <span>超级管理员</span>
+            <span>{roleName()}</span>
           </div>
           <div className={styles.name}>
             <UserOutlined />
-            <span>飘香豆腐</span>
+            <span>{nickName()}</span>
           </div>
           <div className={styles.logout} onClick={this.logout}>
             <LogoutOutlined />
