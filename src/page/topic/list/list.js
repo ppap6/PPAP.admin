22cb474a39cc1f668cb988d7c1f97af1bab2b7f5
“jwchan1 <741755613@qq.com>
2020-08-12 23:49:01 +0800

    feat: topic table

--- src/page/topic/list/list.js
+++ src/page/topic/list/list.js
@@ -1,12 +1,123 @@
-import React from 'react';
-import styles from './list.styl';
+import React from 'react'
+import { Table, Switch, Avatar, Tag, Tooltip, Space, Input, message, Modal } from 'antd'
 
-class List extends React.Component {
-    render() {
-        return (
-            <div>话题列表</div>
-        )
-    }
+import styles from './list.styl'
+
+const { Search } = Input
+const { confirm } = Modal
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+    width: '12%',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    width: '30%',
+    key: 'address',
+  },
+]
+
+const data = [
+  {
+    key: 1,
+    name: 'John Brown sr.',
+    age: 60,
+    address: 'New York No. 1 Lake Park',
+    children: [
+      {
+        key: 11,
+        name: 'John Brown',
+        age: 42,
+        address: 'New York No. 2 Lake Park',
+      },
+      {
+        key: 12,
+        name: 'John Brown jr.',
+        age: 30,
+        address: 'New York No. 3 Lake Park',
+        children: [
+          {
+            key: 121,
+            name: 'Jimmy Brown',
+            age: 16,
+            address: 'New York No. 3 Lake Park',
+          },
+        ],
+      },
+      {
+        key: 13,
+        name: 'Jim Green sr.',
+        age: 72,
+        address: 'London No. 1 Lake Park',
+        children: [
+          {
+            key: 131,
+            name: 'Jim Green',
+            age: 42,
+            address: 'London No. 2 Lake Park',
+            children: [
+              {
+                key: 1311,
+                name: 'Jim Green jr.',
+                age: 25,
+                address: 'London No. 3 Lake Park',
+              },
+              {
+                key: 1312,
+                name: 'Jimmy Green sr.',
+                age: 18,
+                address: 'London No. 4 Lake Park',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    key: 2,
+    name: 'Joe Black',
+    age: 32,
+    address: 'Sidney No. 1 Lake Park',
+  },
+]
+
+// rowSelection objects indicates the need for row selection
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+  onSelect: (record, selected, selectedRows) => {
+    console.log(record, selected, selectedRows);
+  },
+  onSelectAll: (selected, selectedRows, changeRows) => {
+    console.log(selected, selectedRows, changeRows);
+  },
 }
 
-export default List;
+function TreeData() {
+  const [checkStrictly] = React.useState(false);
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>所有话题</div>
+      <Search className={styles.search} placeholder="请输入关键字" enterButton />
+      <Table
+        className={styles.table}
+        columns={columns}
+        rowSelection={{ ...rowSelection, checkStrictly }}
+        dataSource={data}
+      />
+    </div>
+  );
+}
+
+export default TreeData
